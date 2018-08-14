aa426fe4683ed943ecae9cc31b9d0a946ebc8d51
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-14 22:01:37 +0200

    The .htaccess file need to add a line so the post method can work

--- app/Http/Controllers/TheLoaiController.php
+++ app/Http/Controllers/TheLoaiController.php
@@ -4,17 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\TheLoai;
+
 class TheLoaiController extends Controller
 {
     //
     public function getDanhSach()
     {
-    	return view('admin.theloai.danhsach');
+    	$theloai = TheLoai::all();
+    	return view('admin.theloai.danhsach', ['theloai'=> $theloai]);
     }
 
     public function getThem()
     {
     	return view('admin.theloai.them');
+    }
+
+    public function postThem(Request $request)
+    {
+    	echo $request->Ten;
     }
 
     public function getSua()
