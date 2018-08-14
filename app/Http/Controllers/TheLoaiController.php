b5cae65732c6d61a2875d0955926b2c3cc7c60bd
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-14 15:55:19 +0200

    Organize the routes and add <base> tag in index.blade.php so the css can run

---
+++ app/Http/Controllers/TheLoaiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TheLoaiController extends Controller
+{
+    //
+    public function getDanhSach()
+    {
+    	return view('admin.theloai.danhsach');
+    }
+
+    public function getThem()
+    {
+    	return view('admin.theloai.them');
+    }
+
+    public function getSua()
+    {
+    	return view('admin.theloai.sua');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,35 @@ Route::get('thu', function(){
 	return view('admin.theloai.danhsach');
 });
 
+Route::group(['prefix'=>'admin'], function(){
+	//admin/theloai/them...sua...danhsach
+	Route::group(['prefix'=>'theloai'],function(){
+		Route::get('danhsach','TheLoaiController@getDanhSach');
+		Route::get('sua','TheLoaiController@getSua');
+		Route::get('them','TheLoaiController@getThem');
+	});
+
+	Route::group(['prefix'=>'loaitin'],function(){
+		Route::get('danhsach','LoaiTinController@getDanhSach');
+		Route::get('sua','LoaiTinController@getSua');
+		Route::get('them','LoaiTinController@getThem');
+	});
+
+	Route::group(['prefix'=>'tintuc'],function(){
+		Route::get('danhsach','TinTucController@getDanhSach');
+		Route::get('sua','TinTucController@getSua');
+		Route::get('them','TinTucController@getThem');
+	});
+
+	Route::group(['prefix'=>'user'],function(){
+		Route::get('danhsach','UserController@getDanhSach');
+		Route::get('sua','UserController@getSua');
+		Route::get('them','UserController@getThem');
+	});
+
+	Route::group(['prefix'=>'slide'],function(){
+		Route::get('danhsach','SlideController@getDanhSach');
+		Route::get('sua','SlideController@getSua');
+		Route::get('them','SlideController@getThem');
+	});
+});
