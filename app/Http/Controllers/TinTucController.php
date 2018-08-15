25dbf7d45d332b1d51c7d58444a6772820cb954d
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-15 16:37:30 +0200

    Function to add news to database

--- app/Http/Controllers/TinTucController.php
+++ app/Http/Controllers/TinTucController.php
@@ -27,6 +27,39 @@ class TinTucController extends Controller
     public function postThem(Request $request)
     {
     	
+        $tintuc = new TinTuc;
+        $tintuc->TieuDe = $request->TieuDe;
+        $tintuc->TieuDeKhongDau = changeTitle($request->TieuDe);
+        $tintuc->idLoaiTin = $request->LoaiTin;
+        $tintuc->TomTat = $request->TomTat;
+        $tintuc->NoiDung = $request->NoiDung;
+
+        if($request->hasFile('Hinh'))
+        {
+            $file = $request->file('Hinh');
+            $duoi = $file->getClientOriginalExtension();
+            $name = $file->getClientOriginalName();
+            
+            if($duoi != 'jpg' && $duoi != 'png' && $duoi != 'jpeg')
+            {
+                return redirect('admin/tintuc/them')->with('loi','Bạn chỉ được chọn file có đuôi jpg, png, jpeg');
+            }
+
+            while(file_exist("upload/tintuc".$Hinh))
+            {
+                $Hinh = str_random(4)."_".$name;
+            }
+            $file->move("upload/tintuc",$Hinh);
+            $tintuc->Hinh = $Hinh;
+        }   
+        else
+        {
+            $tintuc->Hinh = "";
+        }
+        $tintuc->save();
+
+        return redirect('admin/tintuc/them')->with('thongbao','Thêm tin tức thành công');
+
     }
     //The first array is the error that can be happen, and the second is the error handler
 
--- resources/views/admin/tintuc/them.blade.php
+++ resources/views/admin/tintuc/them.blade.php
@@ -12,7 +12,21 @@
                     </div>
                     <!-- /.col-lg-12 -->
                     <div class="col-lg-7" style="padding-bottom:120px">
-                        <form action="" method="POST">
+                            @if(count($errors) > 0)
+                            <div class="alert alert-danger">
+                                @foreach($errors->all() as $err)
+                                    {{$err}}<br>
+                                @endforeach
+                            </div>
+                            @endif
+
+                            @if(session('thongbao'))
+                                <div class="alert alert-success">
+                                    {{session('thongbao')}}
+                                </div>
+                            @endif
+                        <form action="admin/tintuc/them" method="POST" enctype="multipart/formdata">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
                             <div class="form-group">
                                 <label>Thể loại</label>
                                 <select class="form-control" name="TheLoai" id="TheLoai">
@@ -30,32 +44,34 @@
                                 </select>
                             </div>
                             <div class="form-group">
-                                <label>Category Name</label>
-                                <input class="form-control" name="txtCateName" placeholder="Please Enter Category Name" />
+                                <label>Tiêu đề</label>
+                                <input class="form-control" name="TieuDe" placeholder="Nhập tiêu đề" />
+                            </div>
+                            
+                            <div class="form-group">
+                                <label>Tóm tắt</label>
+                                <textarea name = "TomTat" id="demo" class="form-control ckeditor" rows="3"></textarea>
                             </div>
                             <div class="form-group">
-                                <label>Category Order</label>
-                                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Order" />
+                                <label>Nội dung</label>
+                                <textarea name = "NoiDung" id="demo" class="form-control ckeditor" rows="5"></textarea>
                             </div>
                             <div class="form-group">
-                                <label>Category Keywords</label>
-                                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Keywords" />
+                                <label>Hình ảnh</label>
+                                <input type="file" name="Hinh" las="form-control">
                             </div>
+
                             <div class="form-group">
-                                <label>Category Description</label>
-                                <textarea class="form-control" rows="3"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label>Category Status</label>
+                                <label>Nổi bật</label>
                                 <label class="radio-inline">
-                                    <input name="rdoStatus" value="1" checked="" type="radio">Visible
+                                    <input name="NoiBat" value="0" checked="" type="radio">Không
                                 </label>
                                 <label class="radio-inline">
-                                    <input name="rdoStatus" value="2" type="radio">Invisible
+                                    <input name="NoiBat" value="1" type="radio">Có
                                 </label>
                             </div>
-                            <button type="submit" class="btn btn-default">Category Add</button>
-                            <button type="reset" class="btn btn-default">Reset</button>
+                            <button type="submit" class="btn btn-default">Thêm</button>
+                            <button type="reset" class="btn btn-default">Làm mới</button>
                         <form>
                     </div>
                 </div>
