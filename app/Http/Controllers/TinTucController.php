1ec72ecc7641b26907e68aa8ea97ec50e3cf57bd
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-15 11:22:07 +0200

    Show all the news and its attributes

---
+++ app/Http/Controllers/TinTucController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\TheLoai;
+use App\TinTuc;
+use App\LoaiTin;
+
+class TinTucController extends Controller
+{
+    //
+    public function getDanhSach()
+    {
+    	$tintuc = TinTuc::orderBy('id','DESC')->get();
+        return view('admin.tintuc.danhsach',['tintuc'=>$tintuc]);
+    }
+
+    public function getThem()
+    {
+    	
+    }
+
+    public function postThem(Request $request)
+    {
+    	
+    }
+    //The first array is the error that can be happen, and the second is the error handler
+
+    public function getSua($id)
+    {
+    	
+    }
+
+    public function postSua(Request $request, $id)
+    {
+    	
+    }
+
+    public function getXoa($id)
+    {
+    	
+    }
+}
--- resources/views/admin/tintuc/danhsach.blade.php
+++ resources/views/admin/tintuc/danhsach.blade.php
@@ -6,8 +6,8 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header">Category
-                            <small>List</small>
+                        <h1 class="page-header">Tin tức
+                            <small>Danh sách</small>
                         </h1>
                     </div>
                     <!-- /.col-lg-12 -->
@@ -15,30 +15,39 @@
                         <thead>
                             <tr align="center">
                                 <th>ID</th>
-                                <th>Name</th>
-                                <th>Category Parent</th>
-                                <th>Status</th>
+                                <th>Tiêu đề</th>
+                                <th>Tóm tắt</th>
+                                <th>Thể loại</th>
+                                <th>Loại tin</th>
+                                <th>Số lần xem</th>
+                                <th>Tin nổi bật</th>
                                 <th>Delete</th>
                                 <th>Edit</th>
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach($tintuc as $tt)
                             <tr class="odd gradeX" align="center">
-                                <td>1</td>
-                                <td>Tin Tức</td>
-                                <td>None</td>
-                                <td>Hiện</td>
-                                <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="#"> Delete</a></td>
-                                <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="#">Edit</a></td>
+                                <td>{{$tt->id}}</td>
+                                <td><p>{{$tt->TieuDe}}</p>
+                                    <img width="100px" src="upload/tintuc/{{$tt->Hinh}}" alt="placeholder+image">
+                                </td>
+                                <td>{{$tt->TomTat}}</td>
+                                <td>{{$tt->LoaiTin->TheLoai->Ten}}</td>
+                                <td>{{$tt->LoaiTin->Ten}}</td>
+                                <td>{{$tt->SoLuotXem}}</td>
+                                <td>
+                                    @if($tt->NoiBat == 0)
+                                    {{'Không'}}
+                                    @else
+                                    {{'Có'}}
+                                    @endif
+                                </td>
+                                <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="admin/tintuc/xoa/{{$tt->id}}"> Delete</a></td>
+                                <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="
+                                <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="admin/tintuc/sua/{{$tt->id}}">Edit</a></td>
                             </tr>
-                            <tr class="even gradeC" align="center">
-                                <td>2</td>
-                                <td>Bóng Đá</td>
-                                <td>Thể Thao</td>
-                                <td>Ẩn</td>
-                                <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="#"> Delete</a></td>
-                                <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="#">Edit</a></td>
-                            </tr>
+                           @endforeach
                         </tbody>
                     </table>
                 </div>
