9efcd5b3f274878e53d6f63810e0bcb419aa8619
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-15 11:03:24 +0200

    This is to repaird the genres. I was bored. Sont want to fix anymore

--- app/Http/Controllers/LoaiTinController.php
+++ app/Http/Controllers/LoaiTinController.php
@@ -50,12 +50,33 @@ class LoaiTinController extends Controller
 
     public function getSua($id)
     {
-    	
+    	$theloai = TheLoai::all();
+        $loaitin = LoaiTin::find($id);
+        return view('admin.loaitin.sua',['loaitin'=>$loaitin, 'theloai'=>$theloai]);
+
     }
 
     public function postSua(Request $request, $id)
     {
-    	
+    	$this->validate($request,
+            [
+                'Ten'=>'required|unique:LoaiTin,Ten|min:3|max:100',
+                'TheLoai'=>'required'
+            ]
+            ,
+            [
+                'Ten.require'=>'Bạn chưa nhập tên loại tin',
+                'Ten.unique'=>'Tên loại tin đã tồn tại',
+                'Ten.min'=>'Tên loại tin phải có độ dài từ 3 đến 100 kí tự',
+                'Ten.max'=>'Tên loại tin phải có độ dài từ 3 đến 100 kí tự',
+                'TheLoai.required'=>'Bạn chưa chọn thể loại'
+            ]);
+        $loaitin = LoaiTin::find($id);
+        $loaitin->Ten = $request->Ten;
+        $loaitin->TenKhongDau = changeTitle($request->Ten);
+        $loaitin->idTheLoai = $request->TheLoai;
+        $loaitin->save();
+        return redirect('admin/loaitin/sua/'.$id)->with('thongbao','Bạn đã sửa thành công');
     }
 
     public function getXoa($id)
