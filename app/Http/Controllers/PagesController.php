13b5c4249925eb800df3ddc8952aca503899ab5d
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-16 19:28:55 +0200

    Show the slide and share view it with the pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\TheLoai;
-
+use App\Slide;
 class PagesController extends Controller
 {
     //
     function __construct()
     {
     	$theloai = TheLoai::all();
+    	$slide = Slide::all();
     	view()->share('theloai',$theloai);
+    	view()->share('slide',$slide);
     	//to share the variable 'theloai', that make we dont have to call $theloai = TheLoai::all(); in all other functions
     }
 
