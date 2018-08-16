0be2d4bede9c1ca5ce274f1aa5ac471ff393e1a3
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-08-16 15:39:53 +0200

    Edit and deleter user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,17 +55,51 @@ class UserController extends Controller
 
     public function getSua($id)
     {
-    	
+    	$user = User::find($id);
+        return view('admin.user.sua',['user'=>$user]);
 
     }
 
     public function postSua(Request $request, $id)
     {
-    	
+    	$this->validate($request,
+            [
+                'name'   => 'required|min:3',
+                'email'  => 'required|email|unique:users,email',
+
+            ],[
+                'name.required' =>'Bạn chưa nhập tên người dùng ',
+                'name.min' =>'Tên người dùng phải có ít nhất 3 kí tự',
+            ]);
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->quyen = $request->quyen;
+        
+        if($request->changePassword == "on")
+        {    //changePassword here is the name of the checkbox. Not the id
+            $this->validate($request,
+            [
+                'password' => 'required|min:3|max:32',
+                'passwordAgain' =>'required|same:password'
+
+            ],[
+                'password.required' =>'Bạn chưa nhập mật khẩu',
+                'password.min'=>'Mật khẩu phải có ít nhất 3 kí tự',
+                'password.max'=>'Mật khẩu chỉ được tối đa 32 kí tự',
+                'passwordAgain.required' =>'Bạn chưa nhập lại mật khẩu',
+                'passwordAgain.same' =>'Mật khẩu nhập lại chưa khớp'
+
+            ]);
+            $user->password = bcrypt($request->password);
+        }
+        $user->save();
+        return redirect('admin/user/sua/'.$id)->with('thongbao','Bạn đã sửa thành công');
     }
 
     public function getXoa($id)
     {
-    	
+    	$user = User::find($id);
+        $user->delete();
+        return redirect('admin/user/danhsach')->with('thongbao','Đã xóa người dùng thành công');
     }
 }
